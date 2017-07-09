d4f57a235ae43d4dd2eb39241c325e4d51d04ec4
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-09 11:31:39 +0200

    Added file via upload
    Initial/final stage of mini program.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        //array size
+        char N = Convert.ToChar(Console.ReadLine());
+        string A = Console.ReadLine();
+        string[] aTemp = A.Split(' ', N);
+        Array.Reverse(aTemp);
+        foreach (string substring in aTemp)
+        {
+            Console.Write($"{substring.ToString()} ");
+        }
+    }
+}

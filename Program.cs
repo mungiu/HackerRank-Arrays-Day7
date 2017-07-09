b500288acfad31ae9e4c4b098e3c0c687b19e11c
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-09 13:26:46 +0200

    Update Program.cs
    Finally managed to make the code work.
    Even though i do not use some of the variables i am reading them since HackerRank checks if i read their inputs.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 class Solution
 {
+    public static object RegEx { get; private set; }
+
     static void Main(String[] args)
     {
-        //array size
-        char N = Convert.ToChar(Console.ReadLine());
+        int N = Convert.ToInt32(Console.ReadLine());
         string A = Console.ReadLine();
-        string[] aTemp = A.Split(' ', N);
+
+        string pattern = @"\ ";
+        string[] aTemp = Regex.Split(A, pattern);
         Array.Reverse(aTemp);
-        foreach (string substring in aTemp)
-        {
-            Console.Write($"{substring.ToString()} ");
-        }
+        string aFinal = String.Join(" ", aTemp);
+        
+        Console.Write(aFinal);
     }
 }
